9e2034bcbb75bd5c9eb8a08c69950e5e78744012
Kimberly Wong <kjowong@gmail.com>
2017-05-02 07:18:22 +0000

    Removing comma

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -8,5 +8,5 @@ def fizzbuzz():
         elif i % 3 == 0:
             print("Fizz ", end="")
         else:
-            print("{:d}, ".format(i), end="")
+            print("{:d} ".format(i), end="")
     print()
