23e226ab8a0c0d53563f47e421d5eb21c3325b94
Kimberly Wong <kjowong@gmail.com>
2017-05-10 20:03:50 +0000

    Removing in err

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     while (counter < x):
         try:
             print("{}".format(my_list[counter]), end="")
-        except IndexError as err:
+        except IndexError:
             print()
             return counter
         counter += 1
