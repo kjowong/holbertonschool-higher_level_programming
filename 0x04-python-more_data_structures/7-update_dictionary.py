56a721e843cd7ed9fa67bfdcf48e9727668c6725
Kimberly Wong <kjowong@gmail.com>
2017-05-10 11:35:43 +0000

    remove if statements

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(my_dict, key, value):
-    for key_t in list(my_dict):
-        if key_t != key:
-            my_dict[key] = value
-        if key_t == key:
-            my_dict[key_t] = value
+    my_dict[key] = value
     return my_dict
