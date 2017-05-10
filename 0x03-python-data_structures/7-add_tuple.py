92548d17a50cad7428ed39330ffb2e738f80e9d7
Kimberly Wong <kjowong@gmail.com>
2017-05-10 02:01:48 +0000

    Adding tuple function

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,1 +1,11 @@
-#!/usr/bin/python3
+#/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    tupa_list = list(tuple_a)
+    tupb_list = list(tuple_b)
+    for i in range(0, 2):
+        tupa_list.append(0)
+        tupb_list.append(0)
+    sum1 = tupa_list[0] + tupb_list[0]
+    sum2 = tupa_list[1] + tupb_list[1]
+    final_tuple = (sum1, sum2)
+    return final_tuple
