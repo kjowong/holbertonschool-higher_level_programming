0f4eff39e4dabea7589012f1550f7f6e802ae588
Kimberly Wong <kjowong@gmail.com>
2017-05-10 03:46:23 +0000

    Adding case if list is 0

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    if len(my_list) == 0:
-        return None
+    if my_list is None:
+        return
     new_list = my_list[:]
     for i in my_list:
         if i % 2 == 0:
