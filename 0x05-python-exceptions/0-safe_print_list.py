034765dd067cf2524ce08086043a3dbfd6658475
Kimberly Wong <kjowong@gmail.com>
2017-05-10 18:39:57 +0000

    Removing error

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{}".format(my_list[counter]), end="")
         except IndexError as err:
-            print("Error: {}".format(err))
+            print()
             return counter
         counter += 1
     print()
