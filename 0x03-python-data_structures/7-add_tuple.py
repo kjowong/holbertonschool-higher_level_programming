12a333bd2ce0a0f76d9e18b6cb59d2d063667acc
Kimberly Wong <kjowong@gmail.com>
2017-05-10 02:20:22 +0000

    changing to extend

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,9 +2,8 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     tupa_list = list(tuple_a)
     tupb_list = list(tuple_b)
-    for i in range(0, 2):
-        tupa_list.append(0)
-        tupb_list.append(0)
+    tupa_list.extend([0, 0])
+    tupb_list.extend([0, 0])
     sum1 = tupa_list[0] + tupb_list[0]
     sum2 = tupa_list[1] + tupb_list[1]
     final_tuple = (sum1, sum2)
