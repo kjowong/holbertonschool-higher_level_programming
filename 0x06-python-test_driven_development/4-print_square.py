b5541df5a9573a9600b5f84b6f15768d26b66b05
Kimberly Wong <kjowong@gmail.com>
2017-05-17 17:39:26 +0000

    Changing isnot to is

--- 0x06-python-test_driven_development/4-print_square.py
+++ 0x06-python-test_driven_development/4-print_square.py
@@ -4,7 +4,9 @@
 
 def print_square(size):
     """ print_square function """
-    if not isinstance(size, int) and isinstance(size, float) and size < 0:
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if isinstance(size, float) and size < 0:
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
