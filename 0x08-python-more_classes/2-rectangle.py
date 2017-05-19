ba22e5a57057db0e4c1b0bb5cf6043b453929776
Kimberly Wong <kjowong@gmail.com>
2017-05-19 18:09:37 +0000

    Adding perimeter function

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,1 +1,46 @@
 #/usr/bin/python3
+""" defines a rectangle based on 1-rectangle """
+
+
+class Rectangle:
+    """ defining Rectangle class """
+    def __init__(self, width=0, height=0):
+        """ intializing self """
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """ getting wdith """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ setting width """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """ getting height """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ setting height """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """ defining area """
+        return self.__height * self.__width
+
+    def perimeter(self):
+        """ defining perimeter """
+        return (self.__height * 2) + (self.__width * 2)
