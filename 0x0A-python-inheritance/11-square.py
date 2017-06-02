3e226bbf8746d4d1bfbc8e2cc310111ff83b9242
Kimberly Wong <kjowong@gmail.com>
2017-06-02 01:34:40 +0000

    Adding str

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -9,3 +9,7 @@ class Square(Rectangle):
         self.integer_validator("size", size)
         self.__size = size
         super().__init__(self.__size, self.__size)
+
+    def __str__(self):
+        """ defining str """
+        return "[Square] {}/{}".format(self.__size, self.__size)
