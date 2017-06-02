8702451d340f2fda9dd1943b61d1a3edc56c3d60
Kimberly Wong <kjowong@gmail.com>
2017-06-02 06:24:47 +0000

    testing

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+import sys
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+
+
+file_name = "add_item.json"
+
+if not os.path.isfile(file_name):
+    load_from_json_file(filename)
