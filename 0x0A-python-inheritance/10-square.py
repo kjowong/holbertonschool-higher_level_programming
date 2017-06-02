b77b55dd2ff710181fb14f777ef60bc1b58900fa
Kimberly Wong <kjowong@gmail.com>
2017-06-02 01:21:00 +0000

    checking validate

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -6,5 +6,6 @@ class Square(Rectangle):
     """ defining class Square """
     def __init__(self, size):
         """ initializing self """
+        self.integer_validator("size", size)
         self.__size = size
         super().__init__(self.__size, self.__size)
