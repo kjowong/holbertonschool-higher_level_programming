b530a4f81c26a5c65c6a9e6e4b379ad5f2c308fb
Kimberly Wong <kjowong@gmail.com>
2017-06-03 20:26:34 +0000

    Adding 9

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -4,7 +4,10 @@ save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
 
-file_name = "add_item.json"
 
-if not os.path.isfile(file_name):
-    load_from_json_file(filename)
+def append_arg():
+    new_list = []
+    new_list = load_from_json_file("./add_item.json")
+    for i in range(1, len(sys.argv)):
+        new_list.append(sys.argv[i])
+    save_to_json_file(new_list, "./add_item.json")
