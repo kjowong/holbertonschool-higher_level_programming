ec52b3abc409000f3482d87ea4f7c236e8252e03
Kimberly Wong <kjowong@gmail.com>
2017-07-06 12:07:12 +0000

    Adding more mysql queries

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,2 @@
+-- script that create the MSQL server user user_0d_1
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
---
+++ 0x0E-SQL_more_queries/README.md
@@ -0,0 +1,1 @@
+This project is about privileges and more queries in MySQL
