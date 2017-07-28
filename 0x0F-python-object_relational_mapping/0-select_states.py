d2db21b9c078c5751b8d64e8cd593e80cbf5200d
Kimberly Wong <kjowong@gmail.com>
2017-07-28 10:16:17 +0000

    initial commit

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        password=argv[2],
+        database=argv[3],
+        charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    state_rows = cur.fetchall()
+    for state in state_rows:
+        print(state)
+    cur.close()
+    conn.close()
