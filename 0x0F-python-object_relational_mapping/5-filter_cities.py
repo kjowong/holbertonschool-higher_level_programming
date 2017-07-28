6dd91f114d671b79630cd132dd605e4c4e4bac36
Kimberly Wong <kjowong@gmail.com>
2017-07-28 14:03:12 +0000

    Adding script that takes in arugment

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    """ code to be executed when imported """
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        password=argv[2],
+        database=argv[3],
+        charset="utf8")
+    cur = conn.cursor()
+    cur.execute(
+        "SELECT cities.name FROM cities"
+        " WHERE state_id IN (SELECT states.id from states WHERE"
+        " name = '{}'".format(argv[4]))
+    cities_rows = cur.fetchall()
+    for city in cities_rows:
+        print(city)
+    cur.close()
+    conn.close()
