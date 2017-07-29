b627aa92fb1d5eb47810f05672890f1fe8059caa
Kimberly Wong <kjowong@gmail.com>
2017-07-29 01:14:50 +0000

    Pep8

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -9,6 +9,7 @@ from model_state import Base, State
 class City(Base):
     """ creating class State """
     __tablename__ = 'cities'
-    id = Column(Integer, unique=True, nullable=False, primary_key=True, autoincrement=True)
+    id = Column(Integer, unique=True, nullable=False,
+                primary_key=True, autoincrement=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
