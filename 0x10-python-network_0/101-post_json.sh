005449aca305615ebc772fbe69c83cdfcba60667
Kimberly Wong <kjowong@gmail.com>
2017-08-22 16:19:40 +0000

    Adding silent mode

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a POST to passed URL, displays body of response
-curl -X POST --header "Content-Type: application/json" "$1" -d @"$2"
+curl -sX POST --header "Content-Type: application/json" "$1" -d @"$2"
