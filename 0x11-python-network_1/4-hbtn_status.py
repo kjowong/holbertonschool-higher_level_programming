2d2ef5dc0cc6f71c590923b649c38cb337e6438f
Kimberly Wong <kjowong@gmail.com>
2017-08-24 14:49:59 +0000

    Fixing tabs

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -8,5 +8,5 @@ import requests
 if __name__ == "__main__":
     req = requests.get('https://intranet.hbtn.io/status')
     print("Body response:")
-    print("    - type: {}".format(type(req.text)))
-    print("    - content: {}".format(req.text))
+    print("\t- type: {}".format(type(req.text)))
+    print("\t- content: {}".format(req.text))
