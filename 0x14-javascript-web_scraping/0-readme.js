4fc94093031a28e2a925456f5a7f818f54b1516a
Kimberly Wong <kjowong@gmail.com>
2017-09-18 09:30:28 +0000

    Adding in option

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,7 +2,7 @@
 // script that reads and prints the content of a file
 const fs = require('fs');
 
-fs.readFile(process.argv[2], function (err, file) {
+fs.readFile(process.argv[2], 'utf8', function (err, file) {
   if (err) {
     return console.error(err);
   } else {
--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,7 +2,7 @@
 // script that writes a string to a file
 const fs = require('fs');
 
-fs.writeFile(process.argv[2], process.argv[3], function (err) {
+fs.writeFile(process.argv[2], process.argv[3], 'utf8', function (err) {
   if (err) {
     return console.error(err);
   }
