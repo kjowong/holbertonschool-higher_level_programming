9ba363b48c2d422ee4517542c6f1c1659a679e46
Kimberly Wong <kjowong@gmail.com>
2017-09-18 09:38:17 +0000

    Changed to console.log

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -6,6 +6,6 @@ fs.readFile(process.argv[2], 'utf8', function (err, file) {
   if (err) {
     return console.error(err);
   } else {
-    process.stdout.write(file.toString());
+    console.log(file);
   }
 });
